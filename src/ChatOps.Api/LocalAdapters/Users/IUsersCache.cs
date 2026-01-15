54126e2750fdbe1d4f8f14b6cca5aef32ceb8ff7
Aleksey Pashkov <pashkov.a.n@yandex.ru>
2026-01-15 14:02:36 +0500

    клинап

--- src/ChatOps.Api/LocalAdapters/Users/IUsersCache.cs
+++
@@ -1,9 +0,0 @@
-﻿using ChatOps.Api.Integrations.Telegram.Core;
-
-namespace ChatOps.Api.LocalAdapters.Users;
-
-internal interface IUsersCache
-{
-    void Set(TelegramUser user);
-    TelegramUser? Find(long id);
-}
